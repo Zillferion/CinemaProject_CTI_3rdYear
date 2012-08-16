d6424ed58bf96271bd702f58844b07f83d798ca4
Chasso <drifterskinner@gmail.com>
2012-08-16 09:55:34 +0200

    Updated the database.

--- CinemaDB/sql/debug/CinemaDB.sql
+++ CinemaDB/sql/debug/CinemaDB.sql
@@ -10,8 +10,8 @@ SET NUMERIC_ROUNDABORT OFF;
 
 GO
 :setvar DatabaseName "CinemaDB"
-:setvar DefaultDataPath "c:\Program Files\Microsoft SQL Server\MSSQL10.SQLEXPRESS\MSSQL\DATA\"
-:setvar DefaultLogPath "c:\Program Files\Microsoft SQL Server\MSSQL10.SQLEXPRESS\MSSQL\DATA\"
+:setvar DefaultDataPath "C:\Program Files\Microsoft SQL Server\MSSQL10.SQLEXPRESS\MSSQL\DATA\"
+:setvar DefaultLogPath "C:\Program Files\Microsoft SQL Server\MSSQL10.SQLEXPRESS\MSSQL\DATA\"
 
 GO
 USE [master]
@@ -30,15 +30,15 @@ GO
 
 IF NOT EXISTS (SELECT 1 FROM [master].[dbo].[sysdatabases] WHERE [name] = N'$(DatabaseName)')
 BEGIN
-    RAISERROR(N'You cannot deploy this update script to target SHOGUN-PC\SQLEXPRESS. The database for which this script was built, CinemaDB, does not exist on this server.', 16, 127) WITH NOWAIT
+    RAISERROR(N'You cannot deploy this update script to target EDDIE-PC\SQLEXPRESS. The database for which this script was built, CinemaDB, does not exist on this server.', 16, 127) WITH NOWAIT
     RETURN
 END
 
 GO
 
-IF (@@servername != 'SHOGUN-PC\SQLEXPRESS')
+IF (@@servername != 'EDDIE-PC\SQLEXPRESS')
 BEGIN
-    RAISERROR(N'The server name in the build script %s does not match the name of the target server %s. Verify whether your database project settings are correct and whether your build script is up to date.', 16, 127,N'SHOGUN-PC\SQLEXPRESS',@@servername) WITH NOWAIT
+    RAISERROR(N'The server name in the build script %s does not match the name of the target server %s. Verify whether your database project settings are correct and whether your build script is up to date.', 16, 127,N'EDDIE-PC\SQLEXPRESS',@@servername) WITH NOWAIT
     RETURN
 END
 
@@ -65,145 +65,6 @@ GO
                SELECT * FROM [$(TableName)]					
 --------------------------------------------------------------------------------------
 */
-
-GO
-PRINT N'Dropping DF__tmp_ms_xx__Movie__440B1D61...';
-
-
-GO
-ALTER TABLE [dbo].[tblMovies] DROP CONSTRAINT [DF__tmp_ms_xx__Movie__440B1D61];
-
-
-GO
-PRINT N'Dropping DF__tmp_ms_xx__Creat__44FF419A...';
-
-
-GO
-ALTER TABLE [dbo].[tblMovies] DROP CONSTRAINT [DF__tmp_ms_xx__Creat__44FF419A];
-
-
-GO
-PRINT N'Dropping DF__tmp_ms_xx__IsArc__45F365D3...';
-
-
-GO
-ALTER TABLE [dbo].[tblMovies] DROP CONSTRAINT [DF__tmp_ms_xx__IsArc__45F365D3];
-
-
-GO
-PRINT N'Dropping FK__tlnkMovie__Movie__4D94879B...';
-
-
-GO
-ALTER TABLE [dbo].[tlnkMovieDistributor] DROP CONSTRAINT [FK__tlnkMovie__Movie__4D94879B];
-
-
-GO
-PRINT N'Dropping FK__tlnkScree__Movie__4E88ABD4...';
-
-
-GO
-ALTER TABLE [dbo].[tlnkScreenMovie] DROP CONSTRAINT [FK__tlnkScree__Movie__4E88ABD4];
-
-
-GO
-PRINT N'Dropping CK__tblMovies__BBFC___5165187F...';
-
-
-GO
-ALTER TABLE [dbo].[tblMovies] DROP CONSTRAINT [CK__tblMovies__BBFC___5165187F];
-
-
-GO
-PRINT N'Starting rebuilding table [dbo].[tblMovies]...';
-
-
-GO
-SET TRANSACTION ISOLATION LEVEL SERIALIZABLE;
-
-SET XACT_ABORT ON;
-
-BEGIN TRANSACTION;
-
-CREATE TABLE [dbo].[tmp_ms_xx_tblMovies] (
-    [MovieId]          UNIQUEIDENTIFIER DEFAULT NEWSEQUENTIALID() ROWGUIDCOL NOT NULL,
-    [Created]          DATETIME         DEFAULT GETDATE() NOT NULL,
-    [Name]             NVARCHAR (255)   NOT NULL,
-    [Duration]         INT              NOT NULL,
-    [YearProduced]     INT              NOT NULL,
-    [Director]         NVARCHAR (255)   NOT NULL,
-    [Producer]         NVARCHAR (255)   NOT NULL,
-    [Type]             NVARCHAR (MAX)   NOT NULL,
-    [Description]      TEXT             DEFAULT '' NOT NULL,
-    [ExpectedAudience] NVARCHAR (100)   NOT NULL,
-    [BBFC_Rate]        NVARCHAR (8)     NOT NULL,
-    [IsArchived]       BIT              DEFAULT 0 NOT NULL,
-    PRIMARY KEY CLUSTERED ([MovieId] ASC) WITH (ALLOW_PAGE_LOCKS = ON, ALLOW_ROW_LOCKS = ON, PAD_INDEX = OFF, IGNORE_DUP_KEY = OFF, STATISTICS_NORECOMPUTE = OFF)
-);
-
-IF EXISTS (SELECT TOP 1 1
-           FROM   [dbo].[tblMovies])
-    BEGIN
-        INSERT INTO [dbo].[tmp_ms_xx_tblMovies] ([MovieId], [Created], [Name], [Duration], [YearProduced], [Director], [Producer], [Type], [ExpectedAudience], [BBFC_Rate], [IsArchived])
-        SELECT   [MovieId],
-                 [Created],
-                 [Name],
-                 [Duration],
-                 [YearProduced],
-                 [Director],
-                 [Producer],
-                 [Type],
-                 [ExpectedAudience],
-                 [BBFC_Rate],
-                 [IsArchived]
-        FROM     [dbo].[tblMovies]
-        ORDER BY [MovieId] ASC;
-    END
-
-DROP TABLE [dbo].[tblMovies];
-
-EXECUTE sp_rename N'[dbo].[tmp_ms_xx_tblMovies]', N'tblMovies';
-
-COMMIT TRANSACTION;
-
-SET TRANSACTION ISOLATION LEVEL READ COMMITTED;
-
-
-GO
-PRINT N'Creating On column: MovieId...';
-
-
-GO
-ALTER TABLE [dbo].[tlnkMovieDistributor] WITH NOCHECK
-    ADD FOREIGN KEY ([MovieId]) REFERENCES [dbo].[tblMovies] ([MovieId]) ON DELETE NO ACTION ON UPDATE NO ACTION;
-
-
-GO
-PRINT N'Creating On column: MovieId...';
-
-
-GO
-ALTER TABLE [dbo].[tlnkScreenMovie] WITH NOCHECK
-    ADD FOREIGN KEY ([MovieId]) REFERENCES [dbo].[tblMovies] ([MovieId]) ON DELETE NO ACTION ON UPDATE NO ACTION;
-
-
-GO
-PRINT N'Creating On column: ExpectedAudience...';
-
-
-GO
-ALTER TABLE [dbo].[tblMovies] WITH NOCHECK
-    ADD CHECK (ExpectedAudience = 'Mainstream' OR ExpectedAudience = 'Average' OR ExpectedAudience = 'Below-Average' OR ExpectedAudience = 'Specialist' OR ExpectedAudience = 'Non-Mainstream');
-
-
-GO
-PRINT N'Creating On column: BBFC_Rate...';
-
-
-GO
-ALTER TABLE [dbo].[tblMovies] WITH NOCHECK
-    ADD CHECK (BBFC_Rate='U' OR BBFC_Rate = 'PG' OR BBFC_Rate = '12' OR BBFC_Rate = '12A' OR BBFC_Rate = '15' OR BBFC_Rate = '18' OR BBFC_Rate = 'R18');
-
 
 GO
 /*
@@ -322,67 +183,5 @@ BEGIN
 	INSERT INTO tblScreen (ScreenNum, Seats)
 	VALUES (12, 100)
 END
-
-GO
-PRINT N'Checking existing data against newly created constraints';
-
-
-GO
-USE [$(DatabaseName)];
-
-
-GO
-
-GO
-CREATE TABLE [#__checkStatus] (
-    [Table]      NVARCHAR (270),
-    [Constraint] NVARCHAR (270),
-    [Where]      NVARCHAR (MAX)
-);
-
-SET NOCOUNT ON;
-
-
-GO
-INSERT INTO [#__checkStatus]
-EXECUTE (N'DBCC CHECKCONSTRAINTS (N''[dbo].[tlnkMovieDistributor]'')
-    WITH NO_INFOMSGS');
-
-IF @@ROWCOUNT > 0
-    BEGIN
-        DROP TABLE [#__checkStatus];
-        RAISERROR (N'An error occured while verifying constraints on table [dbo].[tlnkMovieDistributor]', 16, 127);
-    END
-
-
-GO
-INSERT INTO [#__checkStatus]
-EXECUTE (N'DBCC CHECKCONSTRAINTS (N''[dbo].[tlnkScreenMovie]'')
-    WITH NO_INFOMSGS');
-
-IF @@ROWCOUNT > 0
-    BEGIN
-        DROP TABLE [#__checkStatus];
-        RAISERROR (N'An error occured while verifying constraints on table [dbo].[tlnkScreenMovie]', 16, 127);
-    END
-
-
-GO
-INSERT INTO [#__checkStatus]
-EXECUTE (N'DBCC CHECKCONSTRAINTS (N''[dbo].[tblMovies]'')
-    WITH NO_INFOMSGS');
-
-IF @@ROWCOUNT > 0
-    BEGIN
-        DROP TABLE [#__checkStatus];
-        RAISERROR (N'An error occured while verifying constraints on table [dbo].[tblMovies]', 16, 127);
-    END
-
-
-GO
-SET NOCOUNT OFF;
-
-DROP TABLE [#__checkStatus];
-
 
 GO
